fc462c87ab409eaf5dab04b6a5ef90ad59e42916
Yusuf <yusufsaydullayev@gmail.com>
2024-01-24 13:11:24 +0500

    new team members

--- src/components/staticSections/OurtTeam/OurTeam.tsx
+++ src/components/staticSections/OurtTeam/OurTeam.tsx
@@ -6,10 +6,14 @@ import Accauntant from '../../../../public/Team/chief-accountant.png';
 import Mechanic from '../../../../public/Team/chief-mechanical-engineer.png';
 import Cook from '../../../../public/Team/cook.webp';
 import Director from '../../../../public/Team/general-director.png';
-import MarketingManager from '../../../../public/Team/marketing-manager.webp';
-import OfficeManager from '../../../../public/Team/office-manager.webp';
-import SalesManager from '../../../../public/Team/sales-manager.webp';
-import SalesManager2 from '../../../../public/Team/sales-manager2.webp';
+import Olim from '../../../../public/Team/olim-salesmanagerjpg.jpg';
+import Zam from '../../../../public/Team/Abduvali-zam.jpg';
+import Mahliyo from '../../../../public/Team/mahliyo-ofismanager.jpg';
+import Mansur from '../../../../public/Team/mansur-smm.jpg';
+// import MarketingManager from '../../../../public/Team/marketing-manager.webp';
+// import OfficeManager from '../../../../public/Team/office-manager.webp';
+// import SalesManager from '../../../../public/Team/sales-manager.webp';
+// import SalesManager2 from '../../../../public/Team/sales-manager2.webp';
 import TenderManager from '../../../../public/Team/tender-procurement-manager.webp';
 
 export const TeamSection = () => {
@@ -61,52 +65,74 @@ export const TeamSection = () => {
 					<Paragraph size="sm">Главный механик</Paragraph>
 				</div>
 				<div className="flex flex-col items-center">
-					<NextImage
-						width={203}
-						height={203}
-						src={SalesManager}
-						alt="sales-manager"
-					/>
+					<NextImage width={203} height={203} src={Olim} alt="sales-manager" />
 					<Heading className="mt-3" weight="semibold" size="lg" as="h3">
-						Алламбергенов Равшан
+						Олим
 					</Heading>
 					<Paragraph size="sm">Менеджер по продажам</Paragraph>
+					<a
+						href="tel:+99888 140 48 44"
+						target="_blank"
+						rel="noreferrer"
+						className="mt-2"
+					>
+						+99888 140 48 44
+					</a>
+				</div>
+				<div className="flex flex-col items-center">
+					<NextImage width={220} height={220} src={Zam} alt="sales-manager" />
+					<Heading className="mt-3" weight="semibold" size="lg" as="h3">
+						Абдували Раджабов
+					</Heading>
+					<Paragraph size="sm">Зам директор</Paragraph>
+					<a
+						href="tel:+99888 140 48 44"
+						target="_blank"
+						rel="noreferrer"
+						className="mt-2"
+					>
+						+99899 305 01 10
+					</a>
 				</div>
 				<div className="flex flex-col items-center">
 					<NextImage
 						width={203}
 						height={203}
-						src={SalesManager2}
-						alt="sales-manager"
-					/>
-					<Heading className="mt-3" weight="semibold" size="lg" as="h3">
-						Салиева Наргиза
-					</Heading>
-					<Paragraph size="sm">Менеджер по продажам</Paragraph>
-				</div>
-				<div className="flex flex-col items-center">
-					<NextImage
-						width={203}
-						height={203}
-						src={OfficeManager}
+						src={Mahliyo}
 						alt="office-manager"
 					/>
 					<Heading className="mt-3" weight="semibold" size="lg" as="h3">
-						Хасанова Мадина
+						Истамова Махлие
 					</Heading>
 					<Paragraph size="sm">Менеджер офиса</Paragraph>
+					<a
+						href="tel:+99888 140 48 44"
+						target="_blank"
+						rel="noreferrer"
+						className="mt-2"
+					>
+						+99897 007 08 30
+					</a>
 				</div>
 				<div className="flex flex-col items-center">
 					<NextImage
 						width={203}
 						height={203}
-						src={MarketingManager}
+						src={Mansur}
 						alt="marketing-manager"
 					/>
 					<Heading className="mt-3" weight="semibold" size="lg" as="h3">
-						Арипов Абдуллох
+						Мансур Сулаймонов
 					</Heading>
-					<Paragraph size="sm">Менеджер по маркетингу</Paragraph>
+					<Paragraph size="sm">Аккаунт-менеджер СММ</Paragraph>
+					<a
+						href="tel:+99888 140 48 44"
+						target="_blank"
+						rel="noreferrer"
+						className="mt-2"
+					>
+						+99898 306 60 11
+					</a>
 				</div>
 				<div className="flex flex-col items-center">
 					<NextImage width={203} height={203} src={Cook} alt="Повар" />
