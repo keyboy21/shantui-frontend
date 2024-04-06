3fea2c03dc2d572b503682a78e52498fc68ef0e2
Yusufboy Saydullayev <yusufsaydullayev@gmail.com>
2024-04-07 00:36:27 +0500

    Update VideoBlogs.tsx

--- src/app/projects/_sections/VideoBlogs.tsx
+++ src/app/projects/_sections/VideoBlogs.tsx
@@ -4,9 +4,7 @@ import { BASE_URL } from '@/configs/env.config';
 import { Blog, Blogs } from '@/types/api.types';
 
 async function getBlogs(): Promise<Blog[]> {
-	const res = await fetch(`${BASE_URL}/api/blog`, {
-		next: { revalidate: 86400 },
-	});
+	const res = await fetch(`${BASE_URL}/api/blog`);
 	if (!res.ok) {
 		return [];
 	}
